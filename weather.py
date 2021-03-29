897cd7bc28a9ccef59318088ad2260cedc205e77
Abraham <abraham.french@appgate.com>
2021-03-29 22:20:27 +0000

    Latest code

--- weather.py
+++ weather.py
@@ -1,17 +1,32 @@
 import requests
 
-api_key = 'e2205d13dbc7c98a2d6a99c3cff1addd'
-city = "Orlando"
-url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+api_key+'&units=imperial'
+def get_weather_desc_and_temp():
+    api_key = 'e2205d13dbc7c98a2d6a99c3cff1addd'
+    city = "Orlando"
+    url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+api_key+'&units=imperial'
 
-request = requests.get(url)
-json = request.json()
-print(json)
+    request = requests.get(url)
+    json = request.json()
+    #print(json)
 
-description = json.get("weather")[0].get("description")
-print("Today's forecast is", description)
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
 
+    return {
+        'description': description,
+        'temp_min': temp_min,
+        'temp_max': temp_max   
+    }
 
+def main():
+    # Print the results
+    weather_dict = get_weather_desc_and_temp()
+    print("Today's forecast is", weather_dict.get('description'))
+    print("The minimum temperature is", weather_dict.get('temp_min'))
+    print("The maximum temperature is", weather_dict.get('temp_max'))
+
+main()
 
 # temperature =50
 # forecast = "rain"
