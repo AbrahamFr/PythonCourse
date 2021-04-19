dacbd4d3b1b2b718eb8bdb08b305fe2fa44adff1
abraham <abraham@expertcollective.org>
2021-04-18 20:52:00 -0500

    introduce with-block

--- essentials/corepy/recaman_init.py
+++ essentials/corepy/recaman_init.py
@@ -16,9 +16,10 @@ def sequence():
 
 def write_sequence(filename, num):
     """Write Recaman's sequence to a text file."""
-    with open(filename, mode='wt', encoding='utf-8') as f:
+    f = open(filename, mode='wt', encoding='utf-8')
     f.writelines(f"{r}\n"
                   for r in islice(sequence(), num + 1))
+    f.close()
 
 if __name__ == '__main__':
     write_sequence(filename=sys.argv[1], num=int(sys.argv[2]))
--- essentials/corepy/series_list_comprehension.py
+++ essentials/corepy/series_list_comprehension.py
@@ -1,8 +1,11 @@
 import sys
 
 def read_series(filename):
-    with open(filename, mode='rt', encoding='utf-8') as f:  # with-block
-        return [int(line.strip()) for line in f]            # list comprehension
+    try:
+        f = open(filename, mode='rt', encoding='utf-8')
+        return [int(line.strip()) for line in f]    # list comprehension
+    finally:
+        f.close()
 
 def main(filename):
     series = read_series(filename)
